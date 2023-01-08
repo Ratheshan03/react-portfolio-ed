238280df5f30272f2f4cc11d56e63f17be30a05b
99x-dgCandidate-Ratheshan-1642831919908-at-675482622082 <ratheshan.20200170@iit.ac.lk>
2023-01-08 23:12:14 +0530

    Nav-setup updated

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -9,14 +10,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section className="bg-blue-700 min-h-screen">
-          <nav>
-            <h1>Raina Rathe</h1>
-            <ul>
-              <li></li>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">developedbyRathe</h1>
+            <ul className="flex items-center">
               <li>
-                <a href=""></a>
+                <BsFillMoonStarsFill className="cursor-pointer" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
